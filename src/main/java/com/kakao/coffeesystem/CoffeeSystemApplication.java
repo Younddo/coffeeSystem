f96759fb1a87b2e9729aa0c433415ef68ef29099
Younseo Jang <younseojj@gmail.com>
2023-02-06 21:02:16 +0900

    initial commit

---
+++ src/main/java/com/kakao/coffeesystem/CoffeeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.coffeesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakao/coffeesystem/CoffeeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.coffeesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
