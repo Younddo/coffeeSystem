b44704185e8897afb7a6052d445345e63a33c846
Younseo Jang <younseojj@gmail.com>
2023-02-08 20:50:21 +0900

    Feat : MenuController 메뉴 조회기능

--- src/main/java/com/kakao/coffeesystem/controller/MenuController.java
+++ src/main/java/com/kakao/coffeesystem/controller/MenuController.java
@@ -1,9 +1,19 @@
 package com.kakao.coffeesystem.controller;
 
+import com.kakao.coffeesystem.global.GlobalResponseDto;
+import com.kakao.coffeesystem.service.MenuService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MenuController {
+
+    private final MenuService menuService;
+
+    @GetMapping("/menu")
+    public GlobalResponseDto<?> checkMenu() {
+        return menuService.checkMenu();
+    }
 }
