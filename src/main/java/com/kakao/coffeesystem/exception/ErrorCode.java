9fffd74a0fca3b4e9629bc6ebe029b0bb1aafd4e
Younseo Jang <younseojj@gmail.com>
2023-02-08 20:51:35 +0900

    Feat : ErrorCode enum으로

---
+++ src/main/java/com/kakao/coffeesystem/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.kakao.coffeesystem.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    NotFoundUser(HttpStatus.NOT_FOUND.value(), "U001", "유저를 찾을 수 없습니다."),
+    FailToPay(HttpStatus.BAD_REQUEST.value(), "P001", "잔액이 부족합니다.");
+
+    private final int httpStatus;
+    private final String errorCode;
+    private final String message;
+}
