56c74260f7207cbd396df49b65eeab2f6291345c
Younseo Jang <younseojj@gmail.com>
2023-02-08 20:50:36 +0900

    Feat : MenuRepository

--- src/main/java/com/kakao/coffeesystem/repository/MenuRepository.java
+++ src/main/java/com/kakao/coffeesystem/repository/MenuRepository.java
@@ -1,4 +1,11 @@
 package com.kakao.coffeesystem.repository;
 
-public interface MenuRepository {
+import com.kakao.coffeesystem.domain.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
