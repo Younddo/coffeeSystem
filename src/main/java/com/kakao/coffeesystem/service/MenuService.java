76407ce68a6bf0ae30f5d2b6c2b15bdf1dafb6e9
Younseo Jang <younseojj@gmail.com>
2023-02-08 20:50:53 +0900

    Feat : MenuService 메뉴 조회 기능

--- src/main/java/com/kakao/coffeesystem/service/MenuService.java
+++ src/main/java/com/kakao/coffeesystem/service/MenuService.java
@@ -1,4 +1,34 @@
 package com.kakao.coffeesystem.service;
 
+import com.kakao.coffeesystem.domain.Menu;
+import com.kakao.coffeesystem.dto.responseDto.MenuListResponseDto;
+import com.kakao.coffeesystem.dto.responseDto.MenuResponseDto;
+import com.kakao.coffeesystem.global.GlobalResponseDto;
+import com.kakao.coffeesystem.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MenuService {
+
+    private final MenuRepository menuRepository;
+
+    // 메뉴 조회 기능
+    public GlobalResponseDto<?> checkMenu() {
+        List<Menu> menuList = menuRepository.findAll();
+        if(menuList.isEmpty()) {
+            return GlobalResponseDto.fail("조회가능한 메뉴가 없습니다.");
+        }
+        List<MenuResponseDto> menuResponseDtos = new ArrayList<>();
+        for(Menu menu : menuList) {
+            menuResponseDtos.add(new MenuResponseDto(menu));
+        }
+        MenuListResponseDto menuListResponseDto = new MenuListResponseDto(menuResponseDtos);
+        return GlobalResponseDto.ok("메뉴 조회완료", menuListResponseDto);
+    }
+
 }
