f1720396fb4c596fd1913408710d5f576da02039
Younseo Jang <younseojj@gmail.com>
2023-02-08 20:49:32 +0900

    Feat : MenuEntity 생성

--- src/main/java/com/kakao/coffeesystem/domain/Menu.java
+++ src/main/java/com/kakao/coffeesystem/domain/Menu.java
@@ -1,7 +1,25 @@
 package com.kakao.coffeesystem.domain;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
-public class Menu {
+@Getter
+@Setter
+@NoArgsConstructor
+public class Menu extends TimeStamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String coffeeName;
+
+    @Column
+    private Long price;
+
 }
