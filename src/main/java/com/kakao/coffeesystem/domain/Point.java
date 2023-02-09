a3c677be86c7ef3095d5a9dad8dffdca80034144
Younseo Jang <younseojj@gmail.com>
2023-02-09 18:27:43 +0900

    Feat : UserEntity 구현

--- src/main/java/com/kakao/coffeesystem/domain/Point.java
+++
@@ -1,7 +0,0 @@
-package com.kakao.coffeesystem.domain;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Point {
-}
---
+++ src/main/java/com/kakao/coffeesystem/domain/User.java
@@ -0,0 +1,36 @@
+package com.kakao.coffeesystem.domain;
+
+import com.kakao.coffeesystem.dto.requestDto.UserRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.java.Log;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String userNum;
+
+    @Column
+    private Long point;
+
+
+    public User(UserRequestDto userRequestDto) {
+        this.userNum = userRequestDto.getUserNum();
+        this.point = userRequestDto.getChargeAmount();
+    }
+
+    public void update(UserRequestDto userRequestDto) {
+        this.point += userRequestDto.getChargeAmount();
+    }
+}
