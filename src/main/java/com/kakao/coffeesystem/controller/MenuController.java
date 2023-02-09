4b852dea76758d8c809532a01d18898db13d4348
Younseo Jang <younseojj@gmail.com>
2023-02-09 18:25:20 +0900

    Feat : RequestMapping 추가

--- src/main/java/com/kakao/coffeesystem/controller/MenuController.java
+++ src/main/java/com/kakao/coffeesystem/controller/MenuController.java
@@ -4,9 +4,11 @@ import com.kakao.coffeesystem.global.GlobalResponseDto;
 import com.kakao.coffeesystem.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MenuController {
 
