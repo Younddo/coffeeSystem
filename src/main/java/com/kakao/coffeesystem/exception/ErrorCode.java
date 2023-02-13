3d6e3360d2015d9e5288a23dc3c01b653a8c8a73
Younseo Jang <younseojj@gmail.com>
2023-02-13 22:01:47 +0900

    adding ErrorCode

--- src/main/java/com/kakao/coffeesystem/exception/ErrorCode.java
+++ src/main/java/com/kakao/coffeesystem/exception/ErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     NotFoundUser(HttpStatus.NOT_FOUND.value(), "U001", "유저를 찾을 수 없습니다."),
-    FailToPay(HttpStatus.BAD_REQUEST.value(), "P001", "잔액이 부족합니다.");
+    FailToPay(HttpStatus.BAD_REQUEST.value(), "P001", "잔액이 부족합니다."),
+    AlreadyExistName(HttpStatus.BAD_REQUEST.value(), "M001", "이미 존재하는 메뉴명입니다.");
+
 
     private final int httpStatus;
     private final String errorCode;
