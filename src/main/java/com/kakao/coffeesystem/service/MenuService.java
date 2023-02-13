fcd1d4385d2a04a12ce83bdf158b9c67902fb5bd
Younseo Jang <younseojj@gmail.com>
2023-02-13 22:05:07 +0900

    menu 추가기능 Service logic

--- src/main/java/com/kakao/coffeesystem/service/MenuService.java
+++ src/main/java/com/kakao/coffeesystem/service/MenuService.java
@@ -1,8 +1,11 @@
 package com.kakao.coffeesystem.service;
 
 import com.kakao.coffeesystem.domain.Menu;
+import com.kakao.coffeesystem.dto.requestDto.MenuRequestDto;
 import com.kakao.coffeesystem.dto.responseDto.MenuListResponseDto;
 import com.kakao.coffeesystem.dto.responseDto.MenuResponseDto;
+import com.kakao.coffeesystem.exception.CustomException;
+import com.kakao.coffeesystem.exception.ErrorCode;
 import com.kakao.coffeesystem.global.GlobalResponseDto;
 import com.kakao.coffeesystem.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +21,7 @@ public class MenuService {
     private final MenuRepository menuRepository;
 
     // 메뉴 조회 기능
-    public GlobalResponseDto<?> checkMenu() {
+    public GlobalResponseDto<MenuListResponseDto> checkMenu() {
         List<Menu> menuList = menuRepository.findAll();
         if(menuList.isEmpty()) {
             return GlobalResponseDto.fail("조회가능한 메뉴가 없습니다.");
@@ -31,4 +34,13 @@ public class MenuService {
         return GlobalResponseDto.ok("메뉴 조회완료", menuListResponseDto);
     }
 
+    // 메뉴 추가 기능
+    public GlobalResponseDto<MenuResponseDto> addMenu(MenuRequestDto menuRequestDto) {
+        if(menuRepository.existsMenuByCoffeeName(menuRequestDto.getCoffeeName())) {
+            throw new CustomException(ErrorCode.AlreadyExistName);
+        }
+        Menu menu = new Menu(menuRequestDto);
+        menuRepository.save(menu);
+        return GlobalResponseDto.created("메뉴 추가 성공", new MenuResponseDto(menu));
+    }
 }
