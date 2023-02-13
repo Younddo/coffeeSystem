23a853f65bc7d10d136fe5489c56b2001b3244bd
Younseo Jang <younseojj@gmail.com>
2023-02-13 22:03:01 +0900

    NotBlank 주석처리

---
+++ src/main/java/com/kakao/coffeesystem/dto/requestDto/MemberRequestDto.java
@@ -0,0 +1,18 @@
+package com.kakao.coffeesystem.dto.requestDto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class MemberRequestDto {
+
+//    @NotBlank(message = "사용자 식별자는 공백일 수 없습니다.")
+    public String userNum;
+
+//    @NotBlank(message = "충전금액은 공백일 수 없습니다.")
+    public Long chargeAmount;
+
+
+}
