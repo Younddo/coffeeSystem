d12fc9db1eaa329fa12f1ba9b088db9c75d2fd43
Younseo Jang <younseojj@gmail.com>
2023-02-13 22:04:33 +0900

    addMenu logic

--- src/main/java/com/kakao/coffeesystem/controller/MenuController.java
+++ src/main/java/com/kakao/coffeesystem/controller/MenuController.java
@@ -1,11 +1,14 @@
 package com.kakao.coffeesystem.controller;
 
+import com.kakao.coffeesystem.dto.requestDto.MenuRequestDto;
+import com.kakao.coffeesystem.dto.responseDto.MenuListResponseDto;
+import com.kakao.coffeesystem.dto.responseDto.MenuResponseDto;
 import com.kakao.coffeesystem.global.GlobalResponseDto;
 import com.kakao.coffeesystem.service.MenuService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -15,7 +18,12 @@ public class MenuController {
     private final MenuService menuService;
 
     @GetMapping("/menu")
-    public GlobalResponseDto<?> checkMenu() {
+    public GlobalResponseDto<MenuListResponseDto> checkMenu() {
         return menuService.checkMenu();
+    }
+
+    @PostMapping("/menu")
+    public GlobalResponseDto<MenuResponseDto> addMenu(@Valid @RequestBody MenuRequestDto menuRequestDto) {
+        return menuService.addMenu(menuRequestDto);
     }
 }
