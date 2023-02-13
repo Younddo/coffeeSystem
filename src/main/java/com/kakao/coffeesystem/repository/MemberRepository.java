880c7eed5f669aeda1e9a3fa4439e1946d0aca7f
Younseo Jang <younseojj@gmail.com>
2023-02-13 22:02:40 +0900

    existsBy logic

---
+++ src/main/java/com/kakao/coffeesystem/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.kakao.coffeesystem.repository;
+
+import com.kakao.coffeesystem.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findUserByUserNum(String userNum);
+    boolean existsMemberByUserNum(String userNum);
+}
