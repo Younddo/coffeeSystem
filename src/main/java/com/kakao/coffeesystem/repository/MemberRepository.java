535d5fcc96aaebb7f400f33eed1644003effbde2
Younseo Jang <younseojj@gmail.com>
2023-02-15 23:20:32 +0900

    refactor

--- src/main/java/com/kakao/coffeesystem/repository/MemberRepository.java
+++ src/main/java/com/kakao/coffeesystem/repository/MemberRepository.java
@@ -4,7 +4,6 @@ import com.kakao.coffeesystem.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
