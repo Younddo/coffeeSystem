d423d57679c60389e8d482d669a17c8d0cdd4cd4
Younseo Jang <younseojj@gmail.com>
2023-02-18 22:13:42 +0900

    Feat : OrderRequestDto 작성

--- src/main/java/com/kakao/coffeesystem/dto/requestDto/OrderRequestDto.java
+++ src/main/java/com/kakao/coffeesystem/dto/requestDto/OrderRequestDto.java
@@ -1,4 +1,13 @@
 package com.kakao.coffeesystem.dto.requestDto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
 public class OrderRequestDto {
+
+    private Long menuId;
+
+    private String userNum;
+
 }
