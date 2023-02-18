1cf8ff319419d91e05eb5a8b41ef819f78ae0f54
Younseo Jang <younseojj@gmail.com>
2023-02-18 22:13:23 +0900

    Feat : OrderController 작성

--- src/main/java/com/kakao/coffeesystem/controller/OrderController.java
+++ src/main/java/com/kakao/coffeesystem/controller/OrderController.java
@@ -1,6 +1,11 @@
 package com.kakao.coffeesystem.controller;
 
+import com.kakao.coffeesystem.dto.requestDto.OrderRequestDto;
+import com.kakao.coffeesystem.global.GlobalResponseDto;
+import com.kakao.coffeesystem.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class OrderController {
+
+    private final OrderService orderService;
+    @PostMapping("/order")
+    public GlobalResponseDto<?> order(@RequestBody OrderRequestDto orderRequestDto){
+        return orderService.order(orderRequestDto);
+    }
 }
