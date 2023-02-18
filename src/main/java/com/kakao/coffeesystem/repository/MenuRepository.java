a2022971882021c8b1e39b1f65cac4ae06d52390
Younseo Jang <younseojj@gmail.com>
2023-02-18 22:14:48 +0900

    Feat : MenuRepository Optional logic

--- src/main/java/com/kakao/coffeesystem/repository/MenuRepository.java
+++ src/main/java/com/kakao/coffeesystem/repository/MenuRepository.java
@@ -4,7 +4,11 @@ import com.kakao.coffeesystem.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     boolean existsMenuByCoffeeName(String coffeeName);
+
+    Optional<Menu> findMenuById(Long id);
 }
