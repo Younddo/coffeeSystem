bdee1fc1dbb0450ca29f24e733095e316e3f1d19
Younseo Jang <younseojj@gmail.com>
2023-02-18 22:11:40 +0900

    Feat : order Entity 생성

--- src/main/java/com/kakao/coffeesystem/domain/Order.java
+++ src/main/java/com/kakao/coffeesystem/domain/Order.java
@@ -1,10 +1,12 @@
 package com.kakao.coffeesystem.domain;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
 @NoArgsConstructor
 public class Order extends TimeStamped {
 
@@ -12,5 +14,24 @@ public class Order extends TimeStamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
+    private String coffeeName;
+
+    @Column
+    private Long price;
+
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private Menu menu;
+
+
+    public Order(Member member, Menu menu) {
+        this.coffeeName = menu.getCoffeeName();
+        this.price = menu.getPrice();
+        this.member = member;
+        this.menu = menu;
+    }
 
 }
