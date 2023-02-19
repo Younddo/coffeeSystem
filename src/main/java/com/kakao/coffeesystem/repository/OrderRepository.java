53517d81d799ff4dcf4061e498777ce69f8845e6
Younseo Jang <younseojj@gmail.com>
2023-02-19 19:54:28 +0900

    repository annotation

--- src/main/java/com/kakao/coffeesystem/repository/OrderRepository.java
+++ src/main/java/com/kakao/coffeesystem/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.kakao.coffeesystem.repository;
 
 import com.kakao.coffeesystem.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
